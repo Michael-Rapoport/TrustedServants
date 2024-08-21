4cd2dcffb409804c46e77e226cd89f7db966bd26
Agitronics <agitronics@gmail.com>
2024-08-20 18:40:30 -0700

    Create api.js

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
